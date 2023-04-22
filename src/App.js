20772f23ce059945cf1bdba5e39a7f84d390eb7c
Bea <103050835+Beadko@users.noreply.github.com>
2023-04-22 14:18:45 +0700

    pet lists are now loading

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import PetDetailsNotFound from './pages/petDetailsNotFound';
 import Root from './components/root';
 
 // Add react-router-dom imports
-import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route, Outlet } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom';
 
 // create router with JSX Route elements
 const appRouter = createBrowserRouter(createRoutesFromElements(
   <Route path="/" element={<Root />}>
-    <Route path="HomePage" element={ <HomePage /> } />
+    <Route index element={ <HomePage /> }/>
+    <Route path=":type" element={ <HomePage /> }/>
   </Route>
 ));
 
